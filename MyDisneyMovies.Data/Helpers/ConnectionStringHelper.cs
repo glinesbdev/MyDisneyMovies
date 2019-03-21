8e210dd3b03c7f0dbbb3ca1ec9bd2b6c78a62b2e
Bradyn Glines <glinesbdev@gmail.com>
2019-03-21 08:51:17 -0500

    Added a basic database project to the soultion

---
+++ MyDisneyMovies.Data/Helpers/ConnectionStringHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace MyDisneyMovies.Data.Helpers
+{
+    public static class ConnectionStringHelper
+    {
+        public static readonly string MyDisneyMovies = @"Server=localhost;Database=MyDisneyMovies;Trusted_Connection=True;";
+    }
+}
---
+++ MyDisneyMovies.Data/Utils/DatabaseManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+
+namespace MyDisneyMovies.Data
+{
+    public static class DatabaseManager
+    {
+        /// <summary>
+        /// Opens a <see cref="SqlConnection"/>.
+        /// </summary>
+        /// <returns></returns>
+        public static SqlConnection Connect(string connectionString)
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            return connection;
+        }
+    }
+}
